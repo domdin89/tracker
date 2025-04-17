607b1a94ee26064d7d7625de98ae7dfcc6576e29
Domenico Di Nardo <domenico@MacBook-Pro-di-Domenico.local>
2025-04-17 19:49:02 +0200

    initial fix

--- tracker/settings.py
+++ tracker/settings.py
@@ -29,9 +29,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
-    'django_filters',
-    'drf_yasg',
-    'accounts',
     'corsheaders',
     'api',
     'accounts'
